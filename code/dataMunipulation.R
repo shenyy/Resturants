04b6be91180a6d210673beb2eaa4dd005bdec0eb
shenyy <shenyuanyi@hotmail.com>
2015-06-08 10:23:34 -0500

    read from json

--- code/dataMunipulation.R
+++ code/dataMunipulation.R
@@ -1,20 +1,34 @@
-load("data/business.Rdata")
+library("rjson")
 
-n = length(my_results)
-
-data_des = data.frame(id = 1:n)
-data_attr = 
-
-for(i in 1:n){
-  for(cont in names(my_results[[i]])){
-    if(cont != 'attributes'){
-      
-    }else{
-      
-    }
-  }
-}
+dataconv <- function(url){
+  con = file(url, "r")
   
+  input <- readLines(con, -1L)
+  
+  my_results <- lapply(X=input,fromJSON)
+  
+  n = length(my_results)
+  
+  data_des = data.frame(id = 1:n)
+  
+  for(i in 1:n){
+    res_info = unlist(my_results[[i]])
+    for(n in names(res_info)){
+      data_des[i,n] = res_info[n]      
+    }
+  } 
+  close(con)
+  return (data_des)
+}
 
-test = my_results[[1]]
-names(test)
+url1 <- "./data/yelp_academic_dataset_business.json"
+data_biz = dataconv(url1)
+
+url2 <- "./data/yelp_academic_dataset_checkin.json"
+data_check = dataconv(url2)
+
+url3 <- "./data/yelp_academic_dataset_tip.json"
+data_tip = dataconv(url3)
+
+url4 <- "./data/yelp_academic_dataset_user.json"
+data_user = dataconv(url4)
